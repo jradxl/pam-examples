686479060d9b2494bb13ee80a372f57307f1b1e4
John Radley <jradxl@gmail.com>
2021-05-11 00:24:30 +0100

    Update README and upload pam_clear

--- README.md
+++ README.md
@@ -1,29 +1,29 @@
-# pam-examples
-Linux PAM example Modules
+# Pam Examples
+Linux PAM Example Modules
 
-I was interested in how PAM worked, but after reading the documentation, I could work out why I couldn't get two consecutive password prompts, using this change to Common-Auth (Ubuntu 21.04)
+I was interested in how PAM worked, but after reading the documentation, I couldn't work out why I couldn't get two consecutive password prompts, using this change to Common-Auth (Ubuntu 21.04)
 
-# common-auth - After initial comments
-auth    required   pam_pwdfile.so  pwdfile=/root/passwd_file
-auth	  required   pam_clear.so
-# Anything below this comment will be changed by pam-auth-update(8) #
+	# common-auth - After initial comments
+	auth    required    pam_pwdfile.so    pwdfile=/root/passwd_file
+	auth    required    pam_clear.so
+	# Anything below this comment will be changed by pam-auth-update(8) #
 
 Note that it is Debian/Ubuntu that introduces the @include (don't go loading LINUX Pam 1.5.1 in, as it won't work - Linux Pam contains no /etc/pam.d configurations)
 
 I've always puzzled as to why you type your own passwd into sudo!.
 Surely anyone hacking your account can then get to root.
-The arguement goes, that in the days before sudo, the System Admin was persuaded to give out root password, then of course everyone got it!
+The argument goes, that in the days before sudo, the System Admin was persuaded to give out then root password, then of course everyone got it!
 So by inventing sudo and requiring your own password, there was at least some control.
 
 If you search Github and beyond there are many other auth options.
 I choose to try, 
 https://github.com/tiwe-de/libpam-pwdfile
-But the first password prompt passed it to the second line - if they were not the same then auth was denied, hence rather pointless.
+But the first password prompt passed the password to the second line - if they were not the same then auth was denied - they would need to be the same to get access, hence rather pointless.
 
-In asking I got this helpful response, the try_file_pass was now baked in and couldn't be turned off, but the suggestion was that it could be cleared.
+In asking I got this helpful response, the try_first_pass was now baked in and couldn't be turned off, but the suggestion was that it could be cleared.
 Hence pam_clear
 See
 https://github.com/linux-pam/linux-pam/issues/364
 
-
+It's very confusing to get two password prompts, especially for sudo, but it does work.
 
---
+++ pam-clear/src/pam_clear.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <syslog.h>
+#include <security/pam_appl.h>
+#include <security/pam_modules.h>
+#include <security/pam_ext.h>
+
+/*  
+ * Wrapper around conversation function (a callback function provided by
+ * the PAM application to interact with the user)
+ * (based on converse in pam_unix/support.c)
+ * I've taken from otpw
+ */
+static int converse(pam_handle_t *pamh, int nargs,
+                    struct pam_message **message,
+                    struct pam_response **response,
+                    int debug)
+{
+    int retval;
+    struct pam_conv *conv;
+
+    /* get pointer to conversation function */
+    retval = pam_get_item(pamh, PAM_CONV, (const void **) &conv);
+    if (retval != PAM_SUCCESS) {
+        //log_message(LOG_ERR, pamh, "no conversation function: %s", pam_strerror(pamh, retval));
+        pam_syslog(pamh, LOG_ERR, "Pam_Clear: no conversation function:  %s", pam_strerror(pamh, retval));
+        return retval;
+    }
+
+    /* call conversation function */
+    retval = conv->conv(nargs, (const struct pam_message **) message,
+                        response, conv->appdata_ptr);
+
+    if (retval != PAM_SUCCESS) {
+        //log_message(LOG_WARNING, pamh, "conversation function failed: %s", pam_strerror(pamh, retval));
+        pam_syslog(pamh, LOG_INFO, "Pam_Clear: conversation function failed: %s", pam_strerror(pamh, retval));
+    }
+
+    /* propagate error status */
+    return retval;
+}
+
+PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
+    pam_syslog(pamh, LOG_INFO, "Pam_Clear: pam_sm_setcred not used");
+	return PAM_SUCCESS;
+}
+
+PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+    pam_syslog(pamh, LOG_INFO, "Pam_Clear: pam_sm_acct_mgmt not used");
+	return PAM_SUCCESS;
+}
+
+PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
+	int retval;
+    int i;
+
+    pam_syslog(pamh, LOG_INFO, "Pam_Clear: Getting username from PAM stack");
+
+	const char* pUsername;
+	retval = pam_get_user(pamh, &pUsername, "Username: ");
+	if (retval != PAM_SUCCESS) {
+        pam_syslog(pamh, LOG_ERR, "Pam_Clear: Couldn't get username from PAM stack");
+		return retval;
+	}
+    pam_syslog(pamh, LOG_INFO, "Pam_Clear: Username: %s", pUsername);
+
+    retval = pam_set_item(pamh, PAM_AUTHTOK, NULL);
+    if (retval != PAM_SUCCESS) {
+        pam_syslog(pamh, LOG_ERR, "Pam_Clear: Couldn't use Set_Item to Clear PAM_AUTHTOK");
+		return retval;
+	}
+
+    retval = pam_set_item(pamh, PAM_OLDAUTHTOK, NULL);
+    if (retval != PAM_SUCCESS) {
+        pam_syslog(pamh, LOG_ERR, "Pam_Clear: Couldn't use Set_Item to Clear PAM_OLDAUTHTOK");
+		return retval;
+	}
+
+    pam_syslog(pamh, LOG_INFO, "Pam_Clear: Cleared both passwords.");
+	return PAM_SUCCESS;
+}
